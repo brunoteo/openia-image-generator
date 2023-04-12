704b016d4cfde9d782fbaca1e998745f872c5ab5
Matteo Brunetto <brunoteo@hotmail.it>
2023-04-13 00:45:58 +0200

    try to change suggestion api

--- app/api/suggestion/route.ts
+++ app/api/suggestion/route.ts
@@ -1,17 +1,20 @@
+import { NextResponse } from "next/server";
 import openia from "../../../lib/openia";
 
 export async function GET(request: Request) {
   const response = await openia.createCompletion({
-    model: "text-davinci-003",
+      model: "text-davinci-003",
       prompt:
         "Write a random text prompt for DALL·E to generate an image, this prompt will be shown to the user, include details such as the genre and what type of painting it should be, options can include: oil painting, watercolor, photo-realistic, 4k, abstract, modern, black and white etc. Do not wrap the answer in quotes.",
       max_tokens: 100,
       temperature: 0.8,
+
   })
 
   const responseText = response.data.choices[0].text;
 
-  return new Response(JSON.stringify(responseText?.trim()), {
-    status: 200,
-  });
+  return NextResponse.json(responseText?.trim());
+  // return new Response(JSON.stringify(responseText?.trim()), {
+  //   status: 200,
+  // });
 }
